37c39d399eb29d0a77f531911d24ddd712142d09
Soroush <s_falahati@hotmail.com>
2014-06-04 20:51:32 +0430

    Loading animation added, Right and Left panels separated (now they have different scrollbars), NavBar cleanup, Fixed navbar height problem when window width is less than 850 by hiding search field - Fixed requesting multiple pages when there is still one request processing, Fixed back button when it use history object of browser for doing its job, Fixed some forms not treated as ajax form, Fixed cross-panel refresh request, mCustomScrollbar added, Some minor css, javascript and html cleanups

--- js/index.js
+++ js/index.js
@@ -1,22 +1,13 @@
-(function() {
+(function($) {
     "use strict";
 
-    $(function() {
+    $(document).ready(function() {
         requestGet("/left.cgi", 'left');
         requestGet("/right.cgi", 'right');
 
-        // Print errors in a warning box in the right div
-        $.ajaxSetup({
-            error: function(xhr, status, error) {
-                $('#right').prepend("<div class='alert alert-danger'>An AJAX error occured: " + status + "\nError: " + error + "</div>");
-                hideLoading('right');
-                hideLoading('left');
-            }
-        });
-
         // Attach events to left menu a hrefs
         // .leftlink a, .mode a, .linkwithicon a
-        $('#left').on("click", '.leftlink a, .mode a, .linkwithicon a', function(event) {
+        $('#leftContent').on("click", '.leftlink a, .mode a, .linkwithicon a', function(event) {
             var target = $(this).prop('target');
             var href = $(this).prop('href');
             requestGet(href, !target ? 'left' : target);
@@ -25,7 +16,7 @@
 
         // Attach events to hrefs so they load in the right div (mostly right),
         // without interfering with accordions, tabs, tables, etc.
-        $('#right').on("click", 'a:not([href^=""],[href^="mailto:"],[href^="#"])', function(event) {
+        $('#rightContent').on("click", 'a:not([href^=""],[href^="mailto:"],[href^="#"])', function (event) {
             var target = $(this).prop('target');
             var href = $(this).prop('href');
             var onClick = $(this).attr('onclick');
@@ -36,48 +27,14 @@
         });
 
         // Form submit buttons
-        //$('#right').on('submit', '.ui_form', function(event){
-        $('.container-fluid').on('submit', '.ui_form, .navbar-form, .ui_buttons_form', function (event) {
+        $('.container-fluid').on('submit', 'form', function(event) {
             var target = !$(this).prop('target') ? 'right' : $(this).prop('target');
-            showLoading(target);
-            var href = $(this).attr('action');
-            var method = $(this).attr('method');
-            if ($(this).attr('enctype') == 'multipart/form-data') {
-                var formData = new FormData($(this)[0]);
-                $.ajax({
-                    processData: false,
-                    contentType: false,
-                    data: formData,
-                    type: method,
-                    url: href,
-                    success: function (response) {
-                        if (target == 'right') {
-                            changeRight(response, href);
-                        } else {
-                            changeLeft(response, href);
-                        }
-                    }
-                });
-            } else {
-                $.ajax({
-                    data: $(this).serialize(),
-                    type: method,
-                    url: href,
-                    success: function (response) {
-                        if (target == 'right') {
-                            changeRight(response, href);
-                        } else {
-                            changeLeft(response, href);
-                        }
-                    }
-                });
-            }
+            requestPost($(this), target);
             event.preventDefault();
         });
 
         // Config section select dropdown menu XXX make prev and next buts work.
-        //$('#config_section').change(function() { XXX Why doesn't this work?
-        $('#right').on('change', '#config_section', function(event) {
+        $('#rightContent').on('change', '#config_section', function () {
             $('#config_section_form').trigger('submit');
         });
 
@@ -99,117 +56,259 @@
             requestGet(href, !target ? 'right' : target);
             event.preventDefault();
         });
+
+        $(window).resize(function() {
+            calculateColumnsSize();
+        });
+        calculateColumnsSize();
+
+        $(".innerScroll").mCustomScrollbar({
+            theme: "dark-thin",
+            live: true,
+            mouseWheelPixels: 120,
+            scrollButtons: {
+                enable: true
+            }
+        });
     });
 
-    var requestGet = function (href, target) {
+    // Set columns height to correct size
+    var calculateColumnsSize = function() {
+        $('.innerScroll').css(
+            {
+                'max-height': ($(window).height() - $('#topnav').height() - 40) + 'px',
+                'min-height': ($(window).height() - $('#topnav').height() - 40) + 'px'
+            }
+        );
+        $(".innerScroll").mCustomScrollbar("update");
+    };
+
+    // Print errors in a warning box in the right div
+    var printError = function(status, error) {
+        $('#rightContent').prepend("<div class='alert alert-danger'>An AJAX error occured: " + status + "\nError: " + error + "</div>");
+    };
+
+    // Send a GET request and update the panel
+    var requestGet = function(href, target) {
         showLoading(target);
-        if (target == 'right') {
-            $.get(href, function(data) { changeRight(data, href); });
+        if (requestInProgress[target]) {
+            return;
+        }
+        requestInProgress[target] = true;
+        $.ajax({
+            url: href,
+            success: function (response) {
+                hideLoading(target);
+                requestInProgress[target] = false;
+                if (target == 'right') {
+                    changeRight(response, href);
+                } else {
+                    changeLeft(response, href);
+                }
+            },
+            error: function (xhr, status, error) {
+                hideLoading(target);
+                requestInProgress[target] = false;
+                printError(status, error);
+            }
+        });
+    };
+
+    // Send a POST request for a form and update the panel
+    var requestPost = function(form, target) {
+        showLoading(target);
+        if (requestInProgress[target]) {
+            return;
+        }
+        requestInProgress[target] = true;
+        var href = form.attr('action');
+        var method = form.attr('method');
+        if (form.attr('enctype') == 'multipart/form-data') {
+            var formData = new FormData(form[0]);
+            $.ajax({
+                processData: false,
+                contentType: false,
+                data: formData,
+                type: method,
+                url: href,
+                success: function (response) {
+                    hideLoading(target);
+                    requestInProgress[target] = false;
+                    if (target == 'right') {
+                        changeRight(response, href);
+                    } else {
+                        changeLeft(response, href);
+                    }
+                },
+                error: function (xhr, status, error) {
+                    hideLoading(target);
+                    requestInProgress[target] = false;
+                    printError(status, error);
+                }
+            });
         } else {
-            $.get(href, function(data) { changeLeft(data, href); });
+            $.ajax({
+                data: form.serialize(),
+                type: method,
+                url: href,
+                success: function (response) {
+                    hideLoading(target);
+                    requestInProgress[target] = false;
+                    if (target == 'right') {
+                        changeRight(response, href);
+                    } else {
+                        changeLeft(response, href);
+                    }
+                },
+                error: function (xhr, status, error) {
+                    hideLoading(target);
+                    requestInProgress[target] = false;
+                    printError(status, error);
+                }
+            });
         }
     };
 
-    var showLoading = function (target) {
-        if ($('#' + target + ' .loading').is("*")) {
-            $('#' + target + ' .loading').css({ 'display': 'block' });
-        } else {
-            $('#' + target).append('<div class="loading" style="display:block;"></div>');
+    // Make loading hud visible on the desired panel
+    var showLoading = function(target) {
+        if (!$('#' + target + 'Side .loading').is("*")) {
+            $('#' + target + 'Side').append('<div class="loading"></div>');
         }
+        $('#' + target + 'Side .loading').fadeIn(200);
     };
 
-    var hideLoading = function(target) {
-        $('#' + target + ' .loading').css({ 'display': 'none' });
+    // Hide the loading hud - Currently only fires when an error occurred
+    var hideLoading = function (target) {
+        $('#' + target + 'Side .loading').fadeOut(200);
     };
 
     // Change right panel html
     // handle modifying links, and attaching events
     // href - The URL that was loaded that triggered the handler callback
-    var changeRight = function (data, href) {
+    var changeRight = function(data, href) {
         href = fixHrefAddress(href);
 
-        // Prevent bad HTML to break our function - Fix edit virtual server
-        try {
-            $('#right').html(data);
-        } catch (e) { }
+        // Hiding the old content
+        $('#rightContent').fadeTo(200, 0.1, function () {
+            // Prevent bad HTML to break our function - Fix edit virtual server
+            try {
+                $('#rightContent').html(data);
+            } catch (e) {
+            }
 
-        // Hack to fix button table broken html - Fix edit virtual server
-        $('#right .ui_buttons_table button[type=submit]').click(function (event) {
-            if ($(this).parents("form").length == 0) {
-                var form = $(this).closest('tr').prevAll('form').first();
-                var input = $(this).closest('tr').prevAll('input[type=hidden]').first();
-                if (!$.isEmptyObject(form)) {
-                    if (!$.isEmptyObject(input)) {
-                        form.append('<input type="hidden" name="' + input.attr('name') + '" value="' + input.attr('value') + '" />');
+            // Hack to fix button table broken html - Fix edit virtual server
+            $('#rightContent .ui_buttons_table button[type=submit]').click(function (event) {
+                if ($(this).parents("form").length == 0) {
+                    var form = $(this).closest('tr').prevAll('form').first();
+                    var input = $(this).closest('tr').prevAll('input[type=hidden]').first();
+                    if (form.is("*")) {
+                        if (input.is("*")) {
+                            form.append('<input type="hidden" name="' + input.attr('name') + '" value="' + input.attr('value') + '" />');
+                        }
+                        form.append('<input type="hidden" name="' + $(this).attr('name') + '" value="' + $(this).attr('value') + '" />');
+                        form.submit();
+                        event.preventDefault();
                     }
+                }
+            });
+
+            // Hack to fix submit button value when using ajax - Fix multiple submit button for same form
+            $('#rightContent form button[type=submit]').click(function (event) {
+                if ($(this).parents("form").length > 0) {
+                    var form = $(this).closest('form');
                     form.append('<input type="hidden" name="' + $(this).attr('name') + '" value="' + $(this).attr('value') + '" />');
                     form.submit();
                     event.preventDefault();
                 }
+            });
+
+            // Hack to fix relative a urls - Fix back buttons
+            $('#rightContent a:not([href^="http"], [href^="https"], [href^=""], [href^="mailto:"], [href^="#"], [href^="/"], [href^="javascript:"], .ui_link)').each(function () {
+                var lastIndex = href.lastIndexOf("/");
+                if (lastIndex < 9) {
+                    href = href + '/';
+                    lastIndex = href.length;
+                }
+                $(this).attr('href', href.substring(0, lastIndex + 1) + $(this).attr('href'));
+            });
+
+            // Hack to fix back buttons with javascript:history.back(); href
+            $('#rightContent a[href^="javascript:history.back"]').each(function () {
+                $(this).attr('href', lastLoadedUrlRight);
+            });
+
+            // Hack to fix relative form urls - Fix edit virtual server
+            $('#rightContent form:not([action^="http"], [action^="https"], [action^=""], [action^="/"])').each(function () {
+                var lastIndex = href.lastIndexOf("/");
+                if (lastIndex < 9) {
+                    href = href + '/';
+                    lastIndex = href.length;
+                }
+                $(this).attr('action', href.substring(0, lastIndex + 1) + $(this).attr('action'));
+            });
+
+            // We need to refresh left panel
+            if (data.indexOf('top.left.location = top.left.location;') > -1 && lastLoadedUrlLeft != "") {
+                requestGet(lastLoadedUrlLeft, 'left');
             }
-        });
 
-        // Hack to fix submit button value when using ajax - Fix multiple submit button for same form
-        $('#right form button[type=submit]').click(function (event) {
-            var form = $(this).closest('form');
-            form.append('<input type="hidden" name="' + $(this).attr('name') + '" value="' + $(this).attr('value') + '" />');
-            form.submit();
-            event.preventDefault();
-        });
+            calculateColumnsSize();
+            $('#rightSide .innerScroll').mCustomScrollbar("scrollTo", 'top');
+            $('#rightContent').fadeTo(200, 1);
 
-        // Hack to fix relative a urls - Fix back buttons
-        $('#right a:not([href^="http"],[href^="https"],[href^=""],[href^="mailto:"],[href^="#"],[href^="/"],.ui_link)').each(function () {
-            var lastIndex = href.lastIndexOf("/");
-            if (lastIndex < 9) {
-                href = href + '/';
-                lastIndex = href.length;
-            }
-            $(this).attr('href', href.substring(0, lastIndex + 1) + $(this).attr('href'));
+            lastLoadedUrlRight = href;
         });
-
-        // Hack to fix relative form urls - Fix edit virtual server
-        $('#right form:not([action^="http"],[action^="https"],[action^=""],[action^="/"])').each(function () {
-            var lastIndex = href.lastIndexOf("/");
-            if (lastIndex < 9) {
-                href = href + '/';
-                lastIndex = href.length;
-            }
-            $(this).attr('action', href.substring(0, lastIndex + 1) + $(this).attr('action'));
-        });
-
     };
+
+    // A variable to prevent multiple request for one panel
+    var requestInProgress = {
+        "right": false,
+        "left": false
+    };
+
+    // Last loaded url in left panel - GET or POST
+    var lastLoadedUrlLeft = "";
+
+    // Last loaded url in right panel - GET or POST
+    var lastLoadedUrlRight = "";
 
     // Change left panel html
     // handle modifying links, and attaching events
     // href - The URL that was loaded that triggered the handler callback
-    var changeLeft = function (data, href) {
+    var changeLeft = function(data, href) {
         href = fixHrefAddress(href);
 
-        // Prevent bad HTML to break our function
-        try {
-            $('#left').html(data);
-        } catch (e) { }
+        // Hiding the old content
+        $('#leftContent').fadeTo(200, 0.1, function () {
+            // Prevent bad HTML to break our function
+            try {
+                $('#leftContent').html(data);
+            } catch (e) {
+            }
 
-        // Hack to fix domain changer drop down menu
-        $('.domainmenu select').removeAttr('onchange')
-            .off('change')
-            .change(
-                function (event) {
-                    var form = $(this).closest('form');
-                    $.ajax({
-                        processData: false,
-                        contentType: false,
-                        data: form.closest('form').serialize(),
-                        type: form.closest('form').attr('method'),
-                        url: form.closest('form').attr('action'),
-                        success: function (response) {
-                            changeLeft(response, form.closest('form').attr('action'));
-                        }
+            // Hack to fix domain changer drop down menu
+            $('.domainmenu select').removeAttr('onchange')
+                .off('change')
+                .change(
+                    function(event) {
+                        var form = $(this).closest('form');
+                        requestPost(form, 'left');
+                        var rightHref = "/virtual-server/summary_domain.cgi?dom=" + $(this).val();
+                        requestGet(rightHref, 'right');
+                        event.preventDefault();
                     });
-                    var rightHref = "/virtual-server/summary_domain.cgi?dom=" + $(this).val();
-                    requestGet(rightHref, 'right');
-                    event.preventDefault();
-                });
+
+            // We need to refresh right panel
+            if (data.indexOf('top.right.location = top.right.location;') > -1 && lastLoadedUrlRight != "") {
+                requestGet(lastLoadedUrlRight, 'right');
+            }
+
+            calculateColumnsSize();
+            $('#leftSide .innerScroll').mCustomScrollbar("scrollTo", 'top');
+            $('#leftContent').fadeTo(200, 1);
+
+            lastLoadedUrlLeft = href;
+        });
     };
 
     // Return current domain, cross browser version of window.location.origin
@@ -218,7 +317,7 @@
     };
 
     // Makes request URL absolute
-    var fixHrefAddress = function (href) {
+    var fixHrefAddress = function(href) {
         var currentDomain = getCurrentDomain();
         if (href.indexOf('/') == 0) {
             href = currentDomain + href;
@@ -228,4 +327,4 @@
         }
         return href;
     };
-}());
+}(jQuery));
